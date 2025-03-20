743dff17da64c4ef287589133df0b7fa690477bd
lorenal3ita <loh.leitt@fmail.com>
2025-03-20 08:46:58 -0300

    api.1

--- app.py
+++ app.py
@@ -29,8 +29,18 @@ def divisao(num1, num2):
 def subtracao(num1, num2):
     return (f'Soma: {num1} - {num2} = {num1 - num2}')
 
-
-
+@app.route("parImpar:{n1}")
+def parImpar(n1):
+    if n1 % 2 == 0:
+        return True
+    else:
+        return False
+@app.route('/impar:{n1}')
+def impar(n1):
+    if n1 % 2 == 0:
+        return True
+    else:
+        return False
 
 if __name__=='__main__':
   app.run(debug=True)
